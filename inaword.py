74921057f473c044a612b2da8515f832e9a39eb0
Tim Sherratt <tim@discontents.com.au>
2014-04-13 18:33:46 +1000

    Interface tweaks

--- inaword.py
+++ inaword.py
@@ -4,9 +4,12 @@ import pickle
 
 app = Flask(__name__)
 
+#path = ""
+path = "/home/dhistory/webapps/inaword/inaword/"
+
 @app.route("/")
 def home():
-	with open('words_data.pickle', 'rb') as data_file:
+	with open(path + 'words_data.pickle', 'rb') as data_file:
 		data = pickle.load(data_file)
 	return render_template('index.html', data=data)
 
